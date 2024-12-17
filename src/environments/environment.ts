f30bb64827486e0ebcc3f6082a01a6fcc27855fd
Cainan Beltrame <39142115+Cainan94@users.noreply.github.com>
2024-12-17 09:20:46 -0300

    Correção de URL de authenticação

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     apiUrl:'https://equipealcance-core-shv-production.up.railway.app/equipe-alcance-api/v1',
-    authURL:'https://streamhubviewer-auth-api-production.up.railway.app/shv-auth-api/v1/auth'
+    authURL:'https://equipealcance-core-shv-production.up.railway.app/equipe-alcance-api/v1/auth'
 };
